d8d27d0062a443d9c192888940c4c3c7e3dc5975
Jonas Dalethese <99360854+Dalethese@users.noreply.github.com>
2023-03-25 18:42:29 -0300

    arquives

---
+++ scripts/scripts.js
@@ -0,0 +1,34 @@
+const mario = document.querySelector('.mario-walking')
+const pipe = document.querySelector('.pipe')
+const retry = document.querySelector('.retry')
+
+const jump = () => {
+  mario.classList.add('jump')
+
+  setTimeout(() => {
+    mario.classList.remove('jump')
+  }, 500)
+}
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+  if(pipePosition <= 78 && pipePosition > -13 && marioPosition < 108) {
+
+    pipe.style.animation = 'none'
+    pipe.style.left = `${pipePosition}px`
+
+    
+    mario.style.animation = 'none'
+    mario.style.bottom = `${marioPosition}px`
+    mario.src = './images/lose.png'
+    mario.style.width = '60px'
+    mario.style.marginLeft = '20px'
+
+    retry.style.animation = 'retry-anim .5s ease-in-out forwards'
+
+    clearInterval(loop)
+  }
+}, 10)
+
+document.addEventListener('keydown', jump)
